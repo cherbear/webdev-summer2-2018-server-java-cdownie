b6f32e033048bc76be99b7893bfe696df141b28f
Cheryl Downie <cheryldownie@Cheryls-MacBook-Pro-2.local>
2018-07-09 00:36:50 -0400

    Finished setters and getters for the user model

--- src/main/webapp/jquery/models/user.model.client.js
+++ src/main/webapp/jquery/models/user.model.client.js
@@ -1,11 +1,20 @@
 function User(username, password, firstName, lastName) {
   this.username = username;
   this.password = password;
-  // ...same for rest of properties…
+  this.firstName = firstName;
+  this.lastName = lastName;
 
   this.setUsername = setUsername;
   this.getUsername = getUsername;
-  // ...same for rest of properties…
+  
+  this.setPassword = setPassword;
+  this.getPassword = getPassword;
+  
+  this.setFirstName = setFirstName;
+  this.getFirstName = getFirstName;
+  
+  this.setLastName = setLastName;
+  this.getLastName = getLastName;
 
   function setUsername(username) {
     this.username = username;
@@ -13,5 +22,26 @@ function User(username, password, firstName, lastName) {
   function getUsername() {
     return this.username;
   }
-  // ...same for rest of properties…
+  
+  function setPassword(password) {
+	  this.password = password;
+  }
+  function getPassword() {
+	  return this.password;
+  }
+  
+  function setFirstName(firstName) {
+	  this.firstName = firstName;
+  }
+  function getFirstName() {
+	  return this.firstName;
+  }
+  
+  function setLastName(lastName) {
+	  this.lastName = lastName;
+  }
+  function getLastName() {
+	  return this.lastName;
+  }
+  
 }
