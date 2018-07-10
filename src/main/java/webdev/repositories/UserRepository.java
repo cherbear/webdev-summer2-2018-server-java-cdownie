47efabbfdbf109f6c9829e48b8d78354a2dd3728
Cheryl Downie <cheryldownie@Cheryls-MacBook-Pro-2.local>
2018-07-10 14:47:35 -0400

    Added code snippets to user repository and user service

--- src/main/java/webdev/repositories/UserRepository.java
+++ src/main/java/webdev/repositories/UserRepository.java
@@ -1,6 +1,11 @@
 package webdev.repositories;
+
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.CrudRepository;
 
 import webdev.models.User;
-public interface UserRepository extends CrudRepository<User, Integer> {}
+public interface UserRepository extends CrudRepository<User, Integer> {
+	@Query("SELECT user FROM User user WHERE user.username=:username AND user.password=:password")
+	public User findUserByCredentials(@Param("username") String u, @Param("password") String p);
+	
+}
--- src/main/java/webdev/services/UserService.java
+++ src/main/java/webdev/services/UserService.java
@@ -1,5 +1,20 @@
 package webdev.services;
 
-public class UserService {
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+import java.util.*;
 
+import webdev.models.User;
+import webdev.repositories.UserRepository;
+
+@RestController
+public class UserService {
+	@Autowired
+	UserRepository userRepository;
+	
+	@GetMapping("/api/user")	
+	public List<User> findAllUsers() {
+		return userRepository.findAll();
+	}
 }
+
