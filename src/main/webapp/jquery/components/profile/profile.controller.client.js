03cd26409c9457175c77051c37c83a03927f7295
Cheryl Downie <cheryldownie@Cheryls-MacBook-Pro-2.local>
2018-07-11 00:49:50 -0400

    Created profile client-side files

---
+++ src/main/webapp/jquery/components/profile/profile.controller.client.js
@@ -0,0 +1,17 @@
+(function () {
+    var $usernameFld, $passwordFld, $verifyPasswordFld;
+    var $registerBtn;
+    var userService = new UserService();
+    $(main);
+
+    function main() { … }
+    function register() { … }
+})();
+
+
+
+/*
+* Use the sign in, the sign up, and user admin controller code samples to get started. The buttons should invoke event handlers logout() and updateProfile() in the controller, which delegate the actions to corresponding functions in the user service.
+*
+*
+*/
