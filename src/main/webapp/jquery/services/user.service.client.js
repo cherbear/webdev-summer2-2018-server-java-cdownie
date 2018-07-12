7ac6647af082d8e0dacc2c634d00633c539cc570
Cheryl Downie <cheryldownie94@gmail.com>
2018-07-12 05:54:24 -0400

    Testing renderUsers client side

--- src/main/webapp/jquery/services/user.service.client.js
+++ src/main/webapp/jquery/services/user.service.client.js
@@ -4,10 +4,21 @@ function UserServiceClient() {
     this.findUserById = findUserById;
     this.deleteUser = deleteUser;
     this.updateUser = updateUser;
+    this.renderUsers = renderUsers;
     this.register = register;
     this.login = login;
     this.url = 'http://localhost:8080/api/user';
     var self = this;
+    
+    var userServiceClient = new UserServiceClient();
+    
+    function init() {
+        userServiceClient
+          .findAllUsers()
+          .then(renderUsers);
+      }
+      init();
+    
     
     function createUser(user, callback) { 
     	/*
@@ -54,6 +65,50 @@ function UserServiceClient() {
     	 * DELETE /api/user/{userId}
     	 */
     }
+    
+    function renderUsers(users) {
+        console.log(users);
+
+        var tbody = $('tbody');
+        tbody.empty();
+        for(var i=0; i<users.length; i++) {
+          var user = users[i];
+
+          var tr = $('<tr>');
+          var td = $('<td>');
+          td.append(user.username);
+          tr.append(td);
+
+          td = $('<td>');
+          td.append('*******');
+          tr.append(td);
+
+          td = $('<td>');
+          td.append(user.firstName);
+          tr.append(td);
+
+          td = $('<td>');
+          td.append(user.lastName);
+          tr.append(td);
+
+          td = $('<td>');
+          td.append('hello@world.com');
+          tr.append(td);
+
+          td = $('<td>');
+          td.append('Student');
+          tr.append(td);
+
+          td = $('<td>');
+          var deleteBtn = $('<button>DELETE</button>');
+          deleteBtn.click(deleteUser);
+          deleteBtn.attr('id', user.id);
+          td.append(deleteBtn);
+          tr.append(td);
+
+          tr.appendTo(tbody);
+        }
+      }
     
     
     function register() { ... }
